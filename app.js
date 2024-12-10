c740ebaccd280097a50726be78e0b0f308418a30
Tal Finkelshtein <finkelshtein.tal@gmail.com>
2024-12-10 22:57:48 +0200

    init dotenv

--- app.js
+++ app.js
@@ -1,28 +1,12 @@
-// Importing required modules
-const express = require('express');
+const express = require("express");
 const app = express();
-const port = 3000;
+const dotenv = require("dotenv").config();
+const port = process.env.PORT;
 
-// Middleware for parsing JSON bodies
-app.use(express.json());
-
-// Sample route
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-// Another example route
-app.get('/about', (req, res) => {
-  res.send('About Us');
-});
-
-// POST request example
-app.post('/data', (req, res) => {
-  const { name, age } = req.body;
-  res.json({ message: `Received data for ${name}, age ${age}` });
-});
-
-// Starting the server
 app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
+  console.log(`Example app listening at http://localhost:${port}`);
 });
