89ac247514d5a762cf7a383f08831b4cced5fee6
Tal Finkelshtein <finkelshtein.tal@gmail.com>
2025-02-01 14:29:16 +0200

    add properties to post model

--- src/models/posts_model.ts
+++ src/models/posts_model.ts
@@ -1,21 +1,22 @@
 import mongoose from "mongoose";
+import { commentSchema, IComment } from "./comments_model";
 
 export interface IPost {
-  title: string;
   content: string;
   owner: string;
+  likes: number;
+  comments: IComment[];
+  imagePath: string;
 }
 
 const postSchema = new mongoose.Schema<IPost>({
-  title: {
-    type: String,
-    required: true,
-  },
   content: String,
   owner: {
     type: String,
     required: true,
   },
+  imagePath: String,
+  comments: [commentSchema]
 });
 
 const postModel = mongoose.model<IPost>("Posts", postSchema);
